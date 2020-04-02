e891e976fae83a08f6b37613adb816821be81df5
isabelaaug <isabela.augusta@hotmail.com>
2020-04-01 23:11:08 -0300

    Criado movimentos

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32;
 let snake = [];
-
 snake[0] = {
     x: 8 * box,
     y: 8 * box
 }
+let direction = "right";
 
 function criarBG(){
     context.fillStyle = "lightgreen";
@@ -20,5 +20,27 @@ function criarSnake(){
     }
 }
 
-criarBG();
-criarSnake();
+function iniciarJogo(){
+    criarBG();
+    criarSnake();
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if(direction == "right") snakeX += box;
+    if(direction == "left") snakeX -= box;
+    if(direction == "up") snakeY -= box;
+    if(direction == "down") snakeY += box;
+
+    snake.pop();
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newHead);
+
+}
+
+let jogo = setInterval(iniciarJogo, 100)
